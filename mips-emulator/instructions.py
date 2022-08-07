3634a825c1dc22d1f4a32cf78f5101aa5dac6f97
vedeluca <vedeluca@comcast.net>
2022-08-07 17:50:04 -0400

    instructions factory

---
+++ mips-emulator/instructions.py
@@ -0,0 +1,38 @@
+def instruction_factory(opcode="+", rs="$0", rt="$1", immediate=0, rd="$2", shamt=0, funct="?", address="?"):
+    if opcode == "+":
+        return RInstruction(opcode, rs, rt, rd, shamt, funct)
+    elif opcode == "l":
+        return IInstruction(opcode, rs, rt, immediate)
+    elif opcode == "j":
+        return JInstruction(opcode, address)
+    else:
+        raise ValueError(opcode)
+
+
+class Instruction:
+    def __init__(self, opcode):
+        self.opcode = opcode
+
+
+class RInstruction(Instruction):
+    def __init__(self, opcode, rs, rt, rd, shamt, funct):
+        super().__init__(opcode)
+        self.rs = rs
+        self.rt = rt
+        self.rd = rd
+        self.shamt = shamt
+        self.funct = funct
+
+
+class IInstruction(Instruction):
+    def __init__(self, opcode, rs, rt, immediate):
+        super().__init__(opcode)
+        self.rs = rs
+        self.rt = rt
+        self.immediate = immediate
+
+
+class JInstruction(Instruction):
+    def __init__(self, opcode, address):
+        super().__init__(opcode)
+        self.address = address
